9c2de43c3e7a547ce297d36f70b5c712abf670fa
Rajan9721 <rn638600@gmail.com>
2024-01-25 18:36:42 +0530

    Initial commit

---
+++ JobListing/src/test/java/com/rajan/JobListing/JobListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajan.JobListing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
